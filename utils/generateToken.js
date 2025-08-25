2484187e4311f282365772617fb1f750eddee10b
mobilefirstpuneetharaj <puneetharaj.kr@mobilefirstapplications.com>
2025-08-25 19:54:20 +0530

    modified modified cookies options

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,11 +8,11 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: 'lax'
-  };
+ const cookieOptions = {
+  expires: new Date(Date.now() + days * 24 * 60 * 60 * 1000),
+  httpOnly: true,
+  sameSite: "lax",
+};
 
   // ✅ Only secure in production; localhost needs non-secure cookies
   if (process.env.NODE_ENV === 'production') {
