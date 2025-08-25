deb91ffdc57347e63f69048137bacf9d7dc934ec
mobilefirstpuneetharaj <puneetharaj.kr@mobilefirstapplications.com>
2025-08-25 19:11:10 +0530

    modified generate token file

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "1d"
+    expiresIn: process.env.JWT_EXPIRES_IN || 3600
   });
 };
 
