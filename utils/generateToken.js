ccf6cc43ad0878d663db6015e3f0614557e31c73
mobilefirstpuneetharaj <puneetharaj.kr@mobilefirstapplications.com>
2025-08-25 19:26:35 +0530

    modified cookie options

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,7 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRES_IN,10) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     sameSite: 'lax'
