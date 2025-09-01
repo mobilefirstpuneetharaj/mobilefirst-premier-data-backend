dd13cbbb24c53801f90367d8ec2f100e140b5a79
mobilefirstpuneetharaj <puneetharaj.kr@mobilefirstapplications.com>
2025-09-01 13:16:40 +0530

    the development mode url (localhost) url has added into CORS in sever file

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(cookieParser());
 app.use(cors({
   origin:[
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'https://mobilefirst-premier-data-frontend.vercel.app',
     'https://premier-data-frontend-fullstack-training-phase.vercel.app',
     'https://mobilefirst-premier-data-front-mobilefirstpuneetharajs-projects.vercel.app',
